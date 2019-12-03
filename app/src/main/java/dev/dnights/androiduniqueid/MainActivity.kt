223cb78b1a80eb10642bd0f26c2094db0a969a84
hyun dong han <guehgk456@gmail.com>
2019-12-03 21:40:24 +0900

    tv_device_id 추가

--- app/src/main/java/dev/dnights/androiduniqueid/MainActivity.kt
+++ app/src/main/java/dev/dnights/androiduniqueid/MainActivity.kt
@@ -2,11 +2,17 @@ package dev.dnights.androiduniqueid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //tv_device_id.text = UniqueID.getDeviceID(this)
+        //tv_device_id.text = UniqueID.getSimSerialNumber(this)
+        //tv_device_id.text = UniqueID.getAndroidID(this)
+        tv_device_id.text = UniqueID.getWidevineID()
     }
 }
